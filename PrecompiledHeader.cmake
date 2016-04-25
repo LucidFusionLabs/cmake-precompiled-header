578a07f9e7bc4583ee4464082ec280419b32c39a
Lucid Fusion Labs <jmfoutts@yahoo.com>
2016-04-24 21:04:39 -0700

    windows fixes

--- PrecompiledHeader.cmake
+++ PrecompiledHeader.cmake
@@ -1,12 +1,15 @@
 # Function for setting up precompiled headers. Usage:
 #
 #   add_library/executable(target
-#       pchheader.c pchheader.cpp pchheader.h)
+#       pchheader.c pchheader.cpp root/path/to/pchheader.h)
 #
-#   add_precompiled_header(target pchheader.h
-#       [FORCEINCLUDE]
-#       [SOURCE_C pchheader.c]
-#       [SOURCE_CXX pchheader.cpp])
+#   add_precompiled_header(target root/path/to/pchheader.h c++-header
+#       [COMPILE_OPTIONS]
+#       [COMPILE_DEFINITIONS]
+#       [INCLUDE_DIRECTORIES])
+#
+#   use_precompiled_header(target root/path/to/pchheader.h
+#       [FORCEINCLUDE])
 #
 # Options:
 #
@@ -14,18 +17,9 @@
 #   pchheader.h from every source file. Works with both GCC and
 #   MSVC. This is recommended.
 #
-#   SOURCE_C/CXX: Specifies the .c/.cpp source file that includes
-#   pchheader.h for generating the pre-compiled header
-#   output. Defaults to pchheader.c. Only required for MSVC.
-#
-# Caveats:
-#
-#   * MSVC: A source file with the same name as the header must exist
-#     and be included in the target (E.g. header.cpp). Name of file
-#     can be changed using the SOURCE_CXX/SOURCE_C options.
-#
 # License:
 #
+# Copyright (C) 2016 Justin F <justin@lucidfusionlabs.com>
 # Copyright (C) 2009-2013 Lars Christensen <larsch@belunktum.dk>
 #
 # Permission is hereby granted, free of charge, to any person
@@ -57,138 +51,82 @@ if(NOT PCH_PROJECT_BINARY_DIR)
   set(PCH_PROJECT_BINARY_DIR ${CMAKE_BINARY_DIR})
 endif()
 
-macro(set_gcc_pch_vars _input) 
-  get_filename_component(_name ${_input} NAME)
-  get_filename_component(_dir ${_input} DIRECTORY)
+macro(set_pch_vars _input)
   set(_pch_header "${PCH_PROJECT_SOURCE_DIR}/${_input}")
-  set(_pch_binary_dir "${PCH_PROJECT_BINARY_DIR}/${_dir}/${_name}_pch")
-  set(_pchfile "${_pch_binary_dir}/${_name}")
-  set(_output "${_pchfile}.gch")
+  if(USE_PRECOMPILED_HEADERS)
+    get_filename_component(_name ${_input} NAME)
+    get_filename_component(_dir ${_input} DIRECTORY)
+    set(_pch_binary_dir "${PCH_PROJECT_BINARY_DIR}/${_dir}/${_name}_pch")
+    set(_pchfile "${_pch_binary_dir}/${_name}")
+    set(_output "${_pchfile}.gch")
+  else()
+    set(_pchfile ${_pch_header})
+  endif()
 endmacro()
 
 function(add_precompiled_header _target _input _type)
-  if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "Apple")
-    set_gcc_pch_vars(${_input})
+  set_pch_vars(${_input})
+  set(options)
+  set(one_value_args)
+  set(multi_value_args COMPILE_DEFINITIONS COMPILE_OPTIONS INCLUDE_DIRECTORIES)
+  cmake_parse_arguments("" "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+  if(CMAKE_BUILD_TYPE MATCHES Release)
+    set(_compile_options ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE} ${_COMPILE_OPTIONS})
+  else()
+    set(_compile_options ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG} ${_COMPILE_OPTIONS})
+  endif()
+
+  if(USE_PRECOMPILED_HEADERS)
+    set(_compile_options "$<$<BOOL:${_compile_options}>:$<JOIN:${_compile_options},\n>\n>")
+    set(_compile_definitions "$<$<BOOL:${_COMPILE_DEFINITIONS}>:$<JOIN:${_COMPILE_DEFINITIONS},\n>\n>")
+    set(_include_directories "$<$<BOOL:${_INCLUDE_DIRECTORIES}>:-I$<JOIN:${_INCLUDE_DIRECTORIES},\n-I>\n>")
+    set(_pch_flags_file "${_pch_binary_dir}/compile_flags.rsp")
+
     make_directory(${_pch_binary_dir})
+    file(GENERATE OUTPUT ${_pch_flags_file} CONTENT "${_compile_options}${_compile_definitions}${_include_directories}\n")
+    set(_compiler_FLAGS @${_pch_flags_file})
 
-    add_custom_command(
-      OUTPUT "${_pchfile}"
-      COMMAND "${CMAKE_COMMAND}" -E copy "${_pch_header}" "${_pchfile}"
-      DEPENDS "${_pch_header}"
-      IMPLICIT_DEPENDS CXX "${_pch_header}"
-      COMMENT "Updating ${_name}")
+    if(WIN32)
+      add_custom_command(
+        OUTPUT ${_output}
+        COMMAND "${CMAKE_CXX_COMPILER}" ${_compiler_FLAGS} /Fd"${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${_target}.pdb" /TP /c /Fp${_output} /Yc ${_pch_header}
+        DEPENDS ${_pch_header} ${_pch_flags_file}
+        IMPLICIT_DEPENDS CXX ${_pch_header}
+        COMMENT "Precompiling ${_input}")
 
-    if(USE_PRECOMPILED_HEADERS)
-      set(options)
-      set(one_value_args)
-      set(multi_value_args COMPILE_DEFINITIONS COMPILE_OPTIONS INCLUDE_DIRECTORIES)
-      cmake_parse_arguments("" "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-
-      if(CMAKE_BUILD_TYPE MATCHES Release)
-        set(_compile_options ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE} ${_COMPILE_OPTIONS})
-      else()
-        set(_compile_options ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG} ${_COMPILE_OPTIONS})
-      endif()
-      set(_compile_options "$<$<BOOL:${_compile_options}>:$<JOIN:${_compile_options},\n>\n>")
-      set(_compile_definitions "$<$<BOOL:${_COMPILE_DEFINITIONS}>:$<JOIN:${_COMPILE_DEFINITIONS},\n>\n>")
-      set(_include_directories "$<$<BOOL:${_INCLUDE_DIRECTORIES}>:-I$<JOIN:${_INCLUDE_DIRECTORIES},\n-I>\n>")
-
-      set(_pch_flags_file "${_pch_binary_dir}/compile_flags.rsp")
-      file(GENERATE OUTPUT "${_pch_flags_file}" CONTENT "${_compile_definitions}${_include_directories}${_compile_flags}${_compile_options}\n")
-      set(_compiler_FLAGS "@${_pch_flags_file}")
-
+    elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Apple")
       add_custom_command(
         OUTPUT "${_output}"
-        COMMAND "${CMAKE_CXX_COMPILER}" ${_compiler_FLAGS} -x ${_type} -o "${_output}" "${_pchfile}"
-        DEPENDS "${_pchfile}" "${_pch_flags_file}"
+        COMMAND "${CMAKE_CXX_COMPILER}" ${_compiler_FLAGS} -x ${_type} -o "${_output}" "${_pch_header}"
+        DEPENDS ${_pch_header} ${_pch_flags_file}
+        IMPLICIT_DEPENDS CXX ${_pch_header}
         COMMENT "Precompiling ${_input}")
     endif()
   endif()
 endfunction()
 
 function(target_use_precompiled_header _target _input)
-  cmake_parse_arguments(_PCH "FORCEINCLUDE" "SOURCE_CXX:SOURCE_C" "" ${ARGN})
+  set_pch_vars(${_input})
+  cmake_parse_arguments(_PCH "FORCEINCLUDE" "" "" ${ARGN})
 
-  if(MSVC)
-    get_filename_component(_input_we ${_input} NAME_WE)
-    set(_cxx_path "${CMAKE_CFG_INTDIR}/${_target}_cxx_pch")
-    set(_c_path "${CMAKE_CFG_INTDIR}/${_target}_c_pch")
-    make_directory("${_cxx_path}")
-    make_directory("${_c_path}")
-    set(_pch_cxx_header "${_cxx_path}/${_input}")
-    set(_pch_cxx_pch "${_cxx_path}/${_input_we}.pch")
-    set(_pch_c_header "${_c_path}/${_input}")
-    set(_pch_c_pch "${_c_path}/${_input_we}.pch")
-    if(NOT _PCH_SOURCE_CXX)
-      set(_PCH_SOURCE_CXX "${_input_we}.cpp")
-    endif()
-    if(NOT _PCH_SOURCE_C)
-      set(_PCH_SOURCE_C "${_input_we}.c")
-    endif()
-
-    get_target_property(sources ${_target} SOURCES)
-    foreach(_source ${sources})
-      set(_pch_compile_flags "")
-      if(_source MATCHES \\.\(cc|cxx|cpp|c\)$)
-        if(_source MATCHES \\.\(cpp|cxx|cc\)$)
-          set(_pch_header "${_input}")
-          set(_pch "${_pch_cxx_pch}")
-        else()
-          set(_pch_header "${_input}")
-          set(_pch "${_pch_c_pch}")
-        endif()
-        
-        if(_source STREQUAL "${_PCH_SOURCE_CXX}")
-          set(_pch_compile_flags "${_pch_compile_flags} \"/Fp${_pch_cxx_pch}\" /Yc${_input}")
-          set(_pch_source_cxx_found TRUE)
-        elseif(_source STREQUAL "${_PCH_SOURCE_C}")
-          set(_pch_compile_flags "${_pch_compile_flags} \"/Fp${_pch_c_pch}\" /Yc${_input}")
-          set(_pch_source_c_found TRUE)
-        else()
-          if(_source MATCHES \\.\(cpp|cxx|cc\)$)
-            set(_pch_compile_flags "${_pch_compile_flags} \"/Fp${_pch_cxx_pch}\" /Yu${_input}")
-            set(_pch_source_cxx_needed TRUE)
-          else()
-            set(_pch_compile_flags "${_pch_compile_flags} \"/Fp${_pch_c_pch}\" /Yu${_input}")
-            set(_pch_source_c_needed TRUE)
-          endif()
-          if(_PCH_FORCEINCLUDE)
-            set(_pch_compile_flags "${_pch_compile_flags} /FI${_input}")
-          endif(_PCH_FORCEINCLUDE)
-        endif()
-        
-        get_source_file_property(_object_depends "${_source}" OBJECT_DEPENDS)
-        if(NOT _object_depends)
-          set(_object_depends)
-        endif()
-        if(_PCH_FORCEINCLUDE)
-          if(_source MATCHES \\.\(cc|cxx|cpp\)$)
-            list(APPEND _object_depends "${_pch_header}")
-          else()
-            list(APPEND _object_depends "${_pch_header}")
-          endif()
-        endif()
-        
-        set_source_files_properties(${_source} PROPERTIES
-          COMPILE_FLAGS "${_pch_compile_flags}"
-          OBJECT_DEPENDS "${_object_depends}")
+  if(WIN32)
+    set(_pch_compile_flags)
+    if(_PCH_FORCEINCLUDE)
+      if(USE_PRECOMPILED_HEADERS)
+        set(_pch_compile_flags /FI${_pchfile} /Yu${_pchfile} /Fp${_pchfile}.gch)
+      else()
+        set(_pch_compile_flags /FI${_pchfile})
       endif()
-    endforeach()
-
-    if(_pch_source_cxx_needed AND NOT _pch_source_cxx_found)
-      message(FATAL_ERROR "A source file ${_PCH_SOURCE_CXX} for ${_input} is required for MSVC builds. Can be set with the SOURCE_CXX option.")
+    elseif(USE_PRECOMPILED_HEADERS)
+      set(_pch_compile_flags /I ${_pch_binary_dir})
     endif()
-    if(_pch_source_c_needed AND NOT _pch_source_c_found)
-      message(FATAL_ERROR "A source file ${_PCH_SOURCE_C} for ${_input} is required for MSVC builds. Can be set with the SOURCE_C option.")
-    endif()
+    target_compile_options(${_target} PUBLIC ${_pch_compile_flags})
 
   elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"
          OR CMAKE_CXX_COMPILER_ID MATCHES "Apple")
-    set_gcc_pch_vars(${_input})
     set(_pch_compile_flags)
     if(_PCH_FORCEINCLUDE)
-      if(USE_PRECOMPILED_HEADERS AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+      if(USE_PRECOMPILED_HEADERS)
         set(_pch_compile_flags -include-pch ${_pchfile}.gch)
       else()
         set(_pch_compile_flags -include ${_pchfile})
@@ -197,23 +135,24 @@ function(target_use_precompiled_header _target _input)
       set(_pch_compile_flags -I${_pch_binary_dir})
     endif()
     target_compile_options(${_target} PUBLIC ${_pch_compile_flags})
-
-    get_property(_sources TARGET ${_target} PROPERTY SOURCES)
-    foreach(_source ${_sources})
-      if(_source MATCHES \\.\(cc|cxx|cpp|c|m|mm\)$)
-        get_source_file_property(_object_depends "${_source}" OBJECT_DEPENDS)
-        if(NOT _object_depends)
-          set(_object_depends)
-        endif()
-        list(APPEND _object_depends "${_pchfile}")
-        if(USE_PRECOMPILED_HEADERS)
-          list(APPEND _object_depends "${_output}")
-        endif()
-        set_source_files_properties(${_source} PROPERTIES OBJECT_DEPENDS "${_object_depends}")
-      endif()
-    endforeach()
   
   else()
     message(FATAL_ERROR "Unknown compiler ${CMAKE_CXX_COMPILER_ID}")
   endif()
+
+  get_property(_sources TARGET ${_target} PROPERTY SOURCES)
+  foreach(_source ${_sources})
+    if(_source MATCHES \\.\(cc|cxx|cpp|c|m|mm\)$)
+      get_source_file_property(_object_depends "${_source}" OBJECT_DEPENDS)
+      if(NOT _object_depends)
+        set(_object_depends)
+      endif()
+      list(APPEND _object_depends "${_pchfile}")
+      if(USE_PRECOMPILED_HEADERS)
+        list(APPEND _object_depends "${_output}")
+      endif()
+      set_source_files_properties(${_source} PROPERTIES OBJECT_DEPENDS "${_object_depends}")
+    endif()
+  endforeach()
+
 endfunction()
